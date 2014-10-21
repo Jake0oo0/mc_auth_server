a32d39c598afe702740476bd402660ddaad5c7a9
Jake <jake0oo0andminecraft@gmail.com>
2014-10-21 16:04:24 -0500

    Update express files

--- http_listener.js
+++ http_listener.js
@@ -5,11 +5,6 @@ var express = require('express'), app = express()
 , server = http.createServer(app)
 , jade = require('jade');
 
-var sockjs = require('sockjs');
-var chatSocket = sockjs.createServer();
-chatSocket.installHandlers(server, {prefix:'/chat'});
-
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set("view options", { layout: false });
@@ -17,30 +12,7 @@ app.configure(function() {
     app.use(express.static(__dirname + '/public'));
 });
 app.get('/', function(req, res){
-    var tcpURI = null;
-    if ( tcp ) {
-        tcpURI = process.env.RUPPELLS_SOCKETS_FRONTEND_URI;
-    }
-    res.render('home.jade', { 'tcpURI': tcpURI });
+    res.render('home.jade');
 });
 server.listen(http_port);
 console.log("HTTP listening on " + http_port);
-chatSocket.on('connection', function(conn) {
-    var name = "HTTP -> " + conn.remoteAddress + ":" + conn.remotePort;
-    peeps[name] = {
-        'send' : function (message, sender) { conn.write(JSON.stringify({ 'message' : message, 'name' : sender })); }
-    };
-    conn.write(JSON.stringify({ 'message' : "Welcome " + name, 'name' : "Server"}));
-    joined(name);
-    conn.on('data', function (message) {
-        broadcast(message, name);
-    });
-    conn.on('disconnect', function () {
-        left(name);
-    });
-
-    conn.on('data', function(message) {
-        conn.write(message);
-    });
-    conn.on('close', function() {});
-});
